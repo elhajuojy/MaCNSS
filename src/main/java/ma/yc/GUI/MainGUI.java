6e1580753c740ba439f27d7055d580f5de2f981b
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-09-26 11:59:31 +0100

    [ FEATURE ] WORKING WITH HASHMAP IN

--- src/main/java/ma/yc/GUI/MainGUI.java
+++ src/main/java/ma/yc/GUI/MainGUI.java
@@ -4,10 +4,18 @@ import ma.yc.core.Print;
 import ma.yc.dto.DossierDto;
 import ma.yc.service.impl.DossierServiceImpl;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class MainGUI  implements  DisplayGUI{
 
+    private HashMap<Integer , DisplayGUI> options = new HashMap<>();
+
+    public MainGUI() {
+        this.options.put(1, new MainAdministeurGUI());
+        this.options.put(2, new MainAgentGUI());
+        this.options.put(3, new MainPaitentGUI());
+    }
 
     @Override
     public int displayMainOptions(Scanner scanner)  {
@@ -16,22 +24,16 @@ public class MainGUI  implements  DisplayGUI{
         Print.log("2 - are you an agent ? ");
         Print.log("3 - are you a patient ? ");
         Print.log("0 - Exit ");
-        int choice = scanner.nextInt();
 
-        switch (choice) {
-            case 1:
-                new MainAdministeurGUI().displayMainOptions(scanner);
-                break;
-            case 2:
-                    new MainAgentGUI().displayMainOptions(scanner);
-                break;
-            case 3:
-                new MainPaitentGUI().patientDashboard(scanner);
-                break;
-            default:
-                //exit the application in default case
-                break;
+        int choice = scanner.nextInt();
+        if (choice == 0 || choice > 3){
+            Print.log("your choice is not correct");
+            System.exit(0);
+            return 0;
         }
+        DisplayGUI displayGUI = this.options.get(choice);
+        displayGUI.displayMainOptions(scanner);
+
         return choice;
     }
 }
